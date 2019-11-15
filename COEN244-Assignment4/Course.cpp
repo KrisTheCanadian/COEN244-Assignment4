6e5aba8fba7c020e45338475345c84aaba4ff079
Hadrien Ovize <hadrien.ovize@gmail.com>
2019-11-15 17:03:22 -0500

    please
    work

--- COEN244-Assignment4/Course.cpp
+++ COEN244-Assignment4/Course.cpp
@@ -10,3 +10,4 @@ void Course::setcourseName(std::string courseName) {this->m_courseName = courseN
 std::string Course::getcourseNumber() const { return m_courseNumber; };
 void Course::setcourseNumber(std::string cNum) { m_courseNumber = cNum; };
 std::string Course::getcourseName() const { return m_courseName; };
+//please work
